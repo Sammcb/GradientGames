cdb6c5323e8a20218bc6065db7e1ea215a9d0e30
Samuel McBroom <sam@sammcb.com>
2022-02-13 09:45:23 -0500

    Initial Commit

---
+++ GradientGames/GradientGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientGamesApp.swift
+//  GradientGames
+//
+//  Created by Sam McBroom on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GradientGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
